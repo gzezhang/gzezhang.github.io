9c5876e2b18f5d027f640e6f2e7d886d76a807fc
Han Zhang <hanzhang@cs.washington.edu>
2017-04-18 22:41:41 -0700

    d3

---
+++ test.js
@@ -0,0 +1,13 @@
+var a;
+a = "this is javascript";
+b = "will this work?";
+console.log(a);
+console.log(b);
+console.log("37"-7);
+
+var data = [4, 8, 15, 16, 23, 42];
+
+var div = document.createElement("div");
+div.innerHTML = "Hello, Eric!";
+document.body.appendChild(div);
+
