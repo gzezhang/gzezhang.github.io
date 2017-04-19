f5547f9bdd5d0a0db75f902e9e50e6b61cd8ab4c
Han Zhang <hanzhang@cs.washington.edu>
2017-04-18 22:51:20 -0700

    d3

--- test.js
+++ test.js
@@ -5,9 +5,15 @@ console.log(a);
 console.log(b);
 console.log("37"-7);
 
-var data = [4, 8, 15, 16, 23, 42];
+var dataset = [4, 8, 15, 16, 23, 42];
 
 var div = document.createElement("div");
 div.innerHTML = "Hello, Eric!";
 document.body.appendChild(div);
 
+d3.select.("body").selectAll("p")
+   .data(dataset)
+   .enter()
+   .append("p")
+   .text("New paragraph");
+   
