bc08ac9cd0b2501a86f16e6e903fb400eadd5fc9
Han Zhang <hanzhang@cs.washington.edu>
2017-04-27 23:24:17 -0700

    json

--- test.js
+++ test.js
@@ -15,5 +15,17 @@ d3.select("body").selectAll("p")
    .data(dataset)
    .enter()
    .append("p")
-   .text( function(d) { return d; } );
+   .text( function(d) { return d; } )
+   .style("color", "red");
 
+var ob = {
+	name: eric,
+	gender: male,
+	age: 45
+	fun: function() {  return 1;  }
+   };
+
+ob.job = "engineer";
+
+console.log(ob.name);
+console.log(ob.age);   
